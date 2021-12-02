4490f1f434ecb66e283f94bcbf0d5685936756f4
kailen-hargenrader <khargenrader@maret.org>
2021-12-02 17:33:07 -0500

    docs : add javadocs to ImplementedRoom class

--- src/main/java/maze/ImplementedRoom.java
+++ src/main/java/maze/ImplementedRoom.java
@@ -2,17 +2,30 @@ package maze;
 
 import structure.ListInterface;
 import structure.RecursiveLinkedList;
-
+/**
+ * Implements room with Long description, Short description, and a boolean for if the room is an exit.
+ * @author monke
+ *
+ */
 public class ImplementedRoom implements Room {
 	private String Long = new String();
 	private String Short = new String();
 	private Boolean Exit;
 	private RecursiveLinkedList<Room> rooms = new RecursiveLinkedList<Room>();
+	/**
+	 * If no passed variables, make empty descriptions and make room not an exit.
+	 */
 	public ImplementedRoom() {
 		Long = "";
 		Short = "";
 		Exit = false;
 	}
+	/**
+	 * initiate Implemented Room with passed variables
+	 * @param l the long description
+	 * @param s the short description
+	 * @param exit if Implemented Room is an exit
+	 */
 	public ImplementedRoom(String l, String s, Boolean exit) {
 		Long = l;
 		Short = s;
